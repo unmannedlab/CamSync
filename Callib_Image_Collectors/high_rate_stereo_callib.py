f7282a5b9d751719d59743c355cd6dd9361d2386
kololheUSL <kolheshaunak@tamu.edu>
2023-01-31 15:11:33 -0600

    Adding fixed exposure times

--- Callib_Image_Collectors/high_rate_stereo_callib.py
+++ Callib_Image_Collectors/high_rate_stereo_callib.py
@@ -21,6 +21,109 @@ class ChunkDataTypes:
 
 CHOSEN_CHUNK_DATA_TYPE = ChunkDataTypes.IMAGE
 
+def configure_exposure(cam):
+    """
+     This function configures a custom exposure time. Automatic exposure is turned
+     off in order to allow for the customization, and then the custom setting is
+     applied.
+
+     :param cam: Camera to configure exposure for.
+     :type cam: CameraPtr
+     :return: True if successful, False otherwise.
+     :rtype: bool
+    """
+
+    print('*** CONFIGURING EXPOSURE ***\n')
+
+    try:
+        result = True
+
+        # Turn off automatic exposure mode
+        #
+        # *** NOTES ***
+        # Automatic exposure prevents the manual configuration of exposure
+        # times and needs to be turned off for this example. Enumerations
+        # representing entry nodes have been added to QuickSpin. This allows
+        # for the much easier setting of enumeration nodes to new values.
+        #
+        # The naming convention of QuickSpin enums is the name of the
+        # enumeration node followed by an underscore and the symbolic of
+        # the entry node. Selecting "Off" on the "ExposureAuto" node is
+        # thus named "ExposureAuto_Off".
+        #
+        # *** LATER ***
+        # Exposure time can be set automatically or manually as needed. This
+        # example turns automatic exposure off to set it manually and back
+        # on to return the camera to its default state.
+
+        if cam.ExposureAuto.GetAccessMode() != PySpin.RW:
+            print('Unable to disable automatic exposure. Aborting...')
+            return False
+
+        cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
+        print('Automatic exposure disabled...')
+
+        # Set exposure time manually; exposure time recorded in microseconds
+        #
+        # *** NOTES ***
+        # Notice that the node is checked for availability and writability
+        # prior to the setting of the node. In QuickSpin, availability and
+        # writability are ensured by checking the access mode.
+        #
+        # Further, it is ensured that the desired exposure time does not exceed
+        # the maximum. Exposure time is counted in microseconds - this can be
+        # found out either by retrieving the unit with the GetUnit() method or
+        # by checking SpinView.
+
+        if cam.ExposureTime.GetAccessMode() != PySpin.RW:
+            print('Unable to set exposure time. Aborting...')
+            return False
+
+        # Ensure desired exposure time does not exceed the maximum
+        exposure_time_to_set = 10000.0
+        exposure_time_to_set = min(cam.ExposureTime.GetMax(), exposure_time_to_set)
+        cam.ExposureTime.SetValue(exposure_time_to_set)
+        print('Shutter time set to %s us...\n' % exposure_time_to_set)
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
+
+def reset_exposure(cam):
+    """
+    This function returns the camera to a normal state by re-enabling automatic exposure.
+
+    :param cam: Camera to reset exposure on.
+    :type cam: CameraPtr
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+
+        # Turn automatic exposure back on
+        #
+        # *** NOTES ***
+        # Automatic exposure is turned on in order to return the camera to its
+        # default state.
+
+        if cam.ExposureAuto.GetAccessMode() != PySpin.RW:
+            print('Unable to enable automatic exposure (node retrieval). Non-fatal error...')
+            return False
+
+        cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Continuous)
+
+        print('Automatic exposure enabled...')
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
 def compute_timestamp_offset(cam):
     nodemap = cam.GetNodeMap()
     # Latch timestamp. This basically "freezes" the current camera timer into a variable that can be read with
@@ -647,6 +750,7 @@ def run_multiple_cameras(cam_list):
             cam.Init()
             configure_trigger(cam)
             configure_chunk_data(cam.GetNodeMap())
+            configure_exposure(cam)
          
         # Acquire images on all cameras
         result &= acquire_images(cam_list)
@@ -658,6 +762,7 @@ def run_multiple_cameras(cam_list):
         # selecting the camera and then deinitializing it.
         for cam in cam_list:
             reset_trigger(cam)
+            reset_exposure(cam)
 
             # Deinitialize camera
             cam.DeInit()
